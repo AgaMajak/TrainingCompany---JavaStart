a3262eb0b2bd6b97fe21bee9a6a79e8f1cc7c26e
Agnieszka Majak <agamajak@wp.pl>
2020-04-25 18:14:13 +0200

    formatted

--- src/Data/TrainingGroup.java
+++ src/Data/TrainingGroup.java
@@ -11,14 +11,13 @@ public class TrainingGroup {
         this.students = students;
     }
 
-    public void showGroupInfo(){
+    public void showGroupInfo() {
         System.out.println("Nazwa grupy: " + subject);
         System.out.printf("Nauczyciel - %s %s, tel.: %d; Nauczane przedmioty: %s; Wypłata: %.2fzł\n\n", teacher.getFirstName(), teacher.getLastName(), teacher.getPhoneNumber(), teacher.teachingSubject, teacher.salary);
         System.out.println("Studenci w grupie:");
         for (Student student : students) {
             System.out.printf("- %s %s, tel.: %d; Grupy: %s; Czesne: %.2fzł\n", student.getFirstName(), student.getLastName(), student.getPhoneNumber(), student.attendedClasses, student.monthlyCharge);
         }
-
     }
 }
 
